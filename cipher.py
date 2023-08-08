352fba0cdf7e79d6ea28f231e9b1cb8adb01b1dd
Archana Khaiyarguli <133819245+Archana-sth@users.noreply.github.com>
2023-08-07 22:49:25 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,27 +1,19 @@
-# to get a-z alphabet.
-import string
-alphabet = string.ascii_lowercase
 # to get a-z alphabet
 import string 
 alphabet= string.ascii_lowercase
-
-
-def encripted_sentences(plain_sentences, shift):
-    cipher_sentences = ""
-    for char in plain_sentences:
-        if char in alphabet:
-            position=alphabet.index(char)
-            # we need to be within 26 letters.
-            new_position = (position+shift)%26
-            cipher_sentences += alphabet[new_position]
-        else:
-            # number or space will be print as it is.
-            cipher_sentences += char
-    print("the encrypted sentences is :", cipher_sentences)
-
-
+shift = 5
 plain_sentences=input("please enter a sentences : ")
-plain_sentences=plain_sentences.lower()
-shift=int(input("please enter the number of place to shift : "))
-encripted_sentences(plain_sentences, shift)
+
+
+new_message = ""
+for char in plain_sentences:
+    if char in alphabet:
+        position=alphabet.index(char)
+        # we need to be within 26 letters.
+        new_position = (position+shift)%26
+        new_message += alphabet[new_position]
+    else:
+        # number or space will be print as it is.
+        new_message += char
+print("the encrypted sentences is :", new_message)
 
